f2899780006de76bd08c2f337e194f5f2ff86a08
hossein javadi <hosseinj13@yahoo.com>
2024-05-06 16:37:21 +0330

    Create UserService.java

---
+++ src/main/java/org/example/service/user/UserService.java
@@ -0,0 +1,10 @@
+package org.example.service.user;
+
+import org.example.base.service.BaseService;
+import org.example.model.user.User;
+
+import java.sql.SQLException;
+
+public interface UserService extends BaseService<User, Long> {
+    User findByUsername(String username) ;
+}
