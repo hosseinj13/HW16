3bcac24a925ece133e0c9474af8afd563f469214
hossein javadi <hosseinj13@yahoo.com>
2024-05-06 16:38:23 +0330

    Create UserServiceImpl.java

---
+++ src/main/java/org/example/service/user/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package org.example.service.user;
+
+
+import org.example.base.service.BaseServiceImpl;
+import org.example.model.user.User;
+import org.example.repository.user.UserRepository;
+import org.hibernate.SessionFactory;
+
+import java.sql.SQLException;
+
+public class UserServiceImpl extends BaseServiceImpl<User, Long, UserRepository> implements UserService {
+
+    public UserServiceImpl(UserRepository repository) {
+        super(repository);
+    }
+
+
+    @Override
+    public User findByUsername(String username) {
+        return repository.findByUsername(username);
+    }
+}
