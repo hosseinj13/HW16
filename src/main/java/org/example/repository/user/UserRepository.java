7bbd8496fdcbd63276a3fa4d8b94c442ff968b0a
hossein javadi <hosseinj13@yahoo.com>
2024-05-07 12:10:10 +0330

    Fix UserRepository.java

--- src/main/java/org/example/repository/user/UserRepository.java
+++ src/main/java/org/example/repository/user/UserRepository.java
@@ -2,11 +2,12 @@ package org.example.repository.user;
 
 
 import org.example.base.repository.BaseRepository;
+import org.example.enums.UserTypes;
 import org.example.model.user.User;
-
-import java.sql.SQLException;
 
 public interface UserRepository extends BaseRepository<User, Long> {
 
     User findByUsername(String username);
+
+    UserTypes getUserType(Long id);
 }
